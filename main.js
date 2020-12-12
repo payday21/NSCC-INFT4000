1f9da3002466c2a90f03a54daa1fd78b8dbc5f50
payday21 <michaelalleyne28@live.com>
2020-12-11 21:06:03 -0400

    pushing

--- main.js
+++ main.js
@@ -1,43 +1,111 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+//imports
+const path = require('path');
+const url = require('url');
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 1000,
+//deconstruct imports
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+
+//variables for windows
+let mainWindow;
+let addWindow;
+
+//function to create main window
+function createWindow() {
+  mainWindow = new BrowserWindow( {
+    width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+  mainWindow.loadFile('login.html')
   
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  mainWindow.on('closed', function() {
+    app.quit();
+  });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  ipcMain.on('item:add', function(e, name ) {
+    console.log(name);//test data got here to main
+    mainWindow.webContents.send('item:add', name);
+  
+  });
+ 
+  let menu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(menu)
+
+}//end createWindow
+
+//function to create window for Adding
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 700,
+    height: 500,
+    title: 'Add Item',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+//section is tell the addwindow in the 
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  addWindow.on('close', function() {
+    addWindow = null;
+  });
+
+}//end create addWindow
+
+function clearWindow()
+{
+    mainWindow.webContents.send('item:clear');
+}//end function clearWindow
+
+
+//template for menu
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add',
+        click() {createAddWindow()}
+      },
+      {
+        label: 'Clear',
+        click(){clearWindow()}
+      },
+      {
+        label: 'Quit',
+        click(){app.quit()}
+      }
+    ]
+  }
+];
+//dd dev to menu at run time . - note unshift is an array method 
+//places a new element on the front end of an array.
+//All menu items are objects. accelerator can be used for not-keys
+
+mainMenuTemplate.push({
+	label: 'Dev Tools',
+		submenu:[
+		{
+			label: 'Toggle Dev Tools',
+			//accelerator would go here
+			click(item,focusedWindow)
+			{
+				focusedWindow.toggleDevTools();
+			}
+		},
+			{
+				role:'reload'
+			}
+			]
+	
+});
+
+app.on('ready', createWindow)
